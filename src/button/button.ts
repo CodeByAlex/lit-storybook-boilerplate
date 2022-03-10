a3428dd76ebc001878276f3e33a40876c925d0f1
CodeByAlex <wilsonc.alexander@gmail.com>
2022-03-09 20:31:54 -0500

    init

---
+++ src/button/button.ts
@@ -0,0 +1,30 @@
+import { html, TemplateResult, CSSResultGroup, LitElement, css, unsafeCSS, CSSResult } from 'lit';
+import { customElement, property, queryAll, queryAssignedNodes } from 'lit/decorators.js';
+import style from './button.scss';
+export type ButtonVariant =  'primary' | 'success' | 'info' | 'warning' | 'danger';
+/**
+ * The Outline Button component
+ * @slot - The default, and only slot for this element.
+ */
+@customElement('cba-button')
+export class Button extends LitElement {
+
+  static get styles() {
+    return [style];
+  }
+
+  /**
+   * Button variant type that influences the button's style
+   */
+  @property({ type: String })
+  variant: ButtonVariant = 'primary';
+
+  @property() onClick: () => void;
+
+  render(): TemplateResult {
+    return html`<button class="button button-${this.variant}"
+                  .onclick="${this.onClick}">
+                  <slot></slot>
+                </button> `;
+  }
+}
